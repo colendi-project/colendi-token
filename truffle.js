bfbd193e14b55971ece3b239ad53b7ea4365dadb
mdogan93 <mehmetdogan1993@gmail.com>
2019-06-27 10:52:12 +0300

    ApproveAndCall implemented

--- truffle.js
+++ truffle.js
@@ -45,5 +45,27 @@ module.exports = {
             },
             network_id: 1
         },
+    },
+    mocha: {
+        // timeout: 100000
+        reporter: 'eth-gas-reporter',
+        reporterOptions: {
+            currency: 'USD',
+        }
+    },
+
+    // Configure your compilers
+    compilers: {
+        solc: {
+            version: "0.5.8", // Fetch exact version from solc-bin (default: truffle's version)
+            docker: false, // Use "0.5.1" you've installed locally with docker (default: false)
+            settings: { // See the solidity docs for advice about optimization and evmVersion
+                optimizer: {
+                    enabled: false,
+                    runs: 200
+                },
+                evmVersion: "byzantium"
+            }
+        }
     }
 };
